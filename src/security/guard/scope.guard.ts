73f0161cc3061f5d660222c8a04ab03530a18b9f
Danilo <daniloprevides@gmail.com>
2020-02-27 11:53:58 -0300

    Removing comments

--- src/security/guard/scope.guard.ts
+++ src/security/guard/scope.guard.ts
@@ -1,29 +1,28 @@
-import { TokenDto } from '../dto/token.dto';
-import { ClientCredentials } from '../entity/client-credentials.entity';
-import { ScopeEnum } from '../enum/scope.enum';
-import { getRepository } from 'typeorm';
+import { TokenDto } from "../dto/token.dto";
+import { ClientCredentials } from "../entity/client-credentials.entity";
+import { ScopeEnum } from "../enum/scope.enum";
+import { getRepository } from "typeorm";
 import {
   CanActivate,
   ExecutionContext,
   Injectable,
   InternalServerErrorException,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { JwtService } from '@nestjs/jwt';
-import { User } from '../entity/user.entity';
+  UnauthorizedException
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class ScopeGuard implements CanActivate {
   constructor(
     private readonly reflector: Reflector,
-    private readonly jwtService: JwtService,
+    private readonly jwtService: JwtService
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const scopes: ScopeEnum[] = this.reflector.get<ScopeEnum[]>(
-      'scopes',
-      context.getHandler(),
+      "scopes",
+      context.getHandler()
     );
     if (!scopes) {
       return true;
@@ -35,59 +34,34 @@ export class ScopeGuard implements CanActivate {
 
     if (!authorizationHeader) return false;
 
-    const [, token] = authorizationHeader.split(' ');
+    const [, token] = authorizationHeader.split(" ");
     let tokenDto: TokenDto;
     try {
       tokenDto = this.jwtService.verify<TokenDto>(token);
     } catch (e) {
-      if (e.name === 'TokenExpiredError') {
+      if (e.name === "TokenExpiredError") {
         throw new UnauthorizedException(e.message);
       }
       throw new InternalServerErrorException(e);
     }
 
-    if (!tokenDto || !tokenDto.scope || !tokenDto.scope.length || !tokenDto.scope.trim().length){
+    if (
+      !tokenDto ||
+      !tokenDto.scope ||
+      !tokenDto.scope.length ||
+      !tokenDto.scope.trim().length
+    ) {
       throw new UnauthorizedException("User not found");
     }
 
     let hasPermission = false;
     scopes.forEach(i => {
-      if (tokenDto.scope.split(" ").find(s => s == i) != null){
+      if (tokenDto.scope.split(" ").find(s => s == i) != null) {
         hasPermission = true;
       }
     });
 
     return hasPermission;
 
-    // switch(tokenDto.type){
-    //   case "client_credentials": {
-    //     //Getting allowed scopes
-    //     // const clientCredentialsRepository = getRepository<ClientCredentials>("client-credentials");
-    //     // const credential = await clientCredentialsRepository.findOne(tokenDto.id, {
-    //     //   relations: ["scopes"]
-    //     // });
-
-    //     // if (!credential || !credential.scopes || !credential.scopes.length){
-    //     //   throw new UnauthorizedException("User not found");
-    //     // }
-
-    //     let hasPermission = false;
-    //     scopes.forEach(i => {
-    //       if (credential.scopes.find(s => s.name == i) != null){
-    //         hasPermission = true;
-    //       }
-    //     });
-
-    //     return hasPermission;
-    //   }
-      
-    //   default: { //Case user:something
-    //     //Validate scopes on token
-
-    //   }
-    // }
-
-
-   
   }
 }
